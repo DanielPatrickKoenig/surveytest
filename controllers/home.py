c9f7baa4d6bc1c73cb0f8682c36ebfeff99f6153
dan koenig <dangerbird@dans-MBP.attlocal.net>
2022-12-15 16:43:10 -0500

    added react project

--- controllers/home.py
+++ controllers/home.py
@@ -1,3 +1,13 @@
+import os
 @auth.requires_login()
 def index():
-    return dict(message=T('you are home'))
+    dir = 'applications/dndsurvey/static/js/home/build/static'
+    jsItems = os.listdir(dir+'/js')
+    cssItems = os.listdir(dir+'/css')
+    jsFiles = [f for f in jsItems if f[-2:] == 'js' and f[:4] == 'main']
+    cssFiles = [f for f in cssItems if f[-3:] == 'css' and f[:4] == 'main']
+    return dict(
+        message=T('you are home'),
+        cssFile=cssFiles[0],
+        jsFile=jsFiles[0]
+    )
