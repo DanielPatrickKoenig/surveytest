75f0508e817c2ea4243b2e99e1cc8daa4ad78beb
dan koenig <dangerbird@dans-MBP.attlocal.net>
2023-01-02 19:44:17 -0500

    add archive functionality

--- static/js/home/src/api.js
+++ static/js/home/src/api.js
@@ -5,7 +5,7 @@ function getAPIBase(){
     return `/${document.querySelector('#app-name-indicator').getAttribute('app-name')}/`;
 }
 function getLocalStorageProgressKey(){
-    return 'dnd-character-indicator-progress-key-10';
+    return 'dnd-character-indicator-progress-key-13';
 }
 async function load(){
     const response = await axios.get(`${getAPIBase()}home/load_data`);
@@ -18,9 +18,17 @@ async function load(){
     }
 }
 async function save(content){
+    console.log(content);
     const response = await axios.post(`${getAPIBase()}home/save_data`, {content});
     if(response.data?.status !== 'success'){
         window.localStorage.setItem(getLocalStorageProgressKey(), content);
+    }
+}
+async function archive(content){
+    const data_key = `${Math.random().toString().split('.').join('')}-${Math.random().toString().split('.').join('')}-${Math.random().toString().split('.').join('')}`
+    const response = await axios.post(`${getAPIBase()}home/archive_data`, { content, data_key });
+    if(response.data?.status !== 'success'){
+        window.localStorage.setItem(getLocalStorageProgressKey(), null);
     }
 }
 async function getQuestions(){
@@ -44,6 +52,8 @@ async function updateProgress(progressData, questionIndexes, currentIndex){
     dataList[currentIndex] = progressData;
     // dataList.push(progressData);
     await save([ dataList.join(itemSplitter), indexes, progress ].join(sectionSplitter));
+    if(questionIndexes.length - 1 === currentIndex) await archive([ dataList.join(itemSplitter), indexes, progress ].join(sectionSplitter));
+
 }
 async function parseContent(){
     let progress = [];
@@ -82,4 +92,4 @@ async function getResults(content){
     const response = await axios.get(`${getAPIBase()}home/process_results`, {params:{content, section_splitter: sectionSplitter, item_splitter: itemSplitter, value_splitter: '-'}});
     return response.data;
 }
-export { getAPIBase, load, save, getQuestions, questionsLoaded, updateProgress, parseContent, transferLocalDataToFile, getResults };
+export { getAPIBase, load, save, archive, getQuestions, questionsLoaded, updateProgress, parseContent, transferLocalDataToFile, getResults };
